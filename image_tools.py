f6fb39514da27fefe6dc54a52cd22437e7e39cd9
pieceofcheese <yinlinhai@gmail.com>
2019-09-26 15:49:44 -0400

    training works on 1 picture probably

--- image_tools.py
+++ image_tools.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import os
-from numpy import array
+import numpy as np
 
 ImagePath = "images/"
 
@@ -21,11 +21,55 @@ def get_images(rootPath=ImagePath):
 def resizeImage(image, scale):
     width,height = image.size
     image.resize((width*scale, height*scale))
+
+def save_as_image(arr, filename):
+    a = Image.fromarray(arr)
+    a.save(filename)
+
+def get_patches(np_image, patch_size, stride=1):
+    """ expects a 2d array contianing data organized in a row, column order
+        returns patches row first
+    """
     
+    if (stride < 1):
+        raise IndexError()
+
+    height, width = np_image.shape[:2]
+    
+    patch_height, patch_width = patch_size
+    
+    if patch_height > height or patch_width > width:
+        raise Exception("Patch is bigger than image")
+    
+    patches_shape = merge_tuples((((height - patch_height)//stride + 1)* ((width - patch_width)//stride + 1)),
+                                 patch_size,
+                                 np_image.shape[2:])
+    
+    patches = np.zeros(patches_shape)
+    
+    try:
+        for i in range(0, height - patch_height + 1, stride):
+            for j in range(0, width - patch_width + 1, stride):
+                #print("{},{}".format(i,j))
+                patches[i//stride*((width - patch_width)//stride + 1) + j//stride] = np_image[i:i+patch_height, j:j+patch_width]
+                
+    except TypeError:
+        raise TypeError("ERROR: One of your inputs is of the wrong type. Most likely this is the stride")
+    except IndexError:
+        print("{},{}".format(i,j))
+        print(patches_shape)
+        raise IndexError()
+        
+    return patches
+
+def merge_tuples(*t):
+    """ taken from https://stackoverflow.com/questions/14745199/how-to-merge-two-tuples-in-python/14745275#14745275"""
+    return tuple(j for i in (t) for j in (i if isinstance(i, tuple) else (i,)))
+
 def get_patch(image, coord_start, height, width):
     x,y = coord_start
     patch = []
-    data = image.getdata(0)
+    
     for i in range(height):
         index = xyConversion(x,y+i,image)
         tArray = []
--- sparse.py
+++ sparse.py
@@ -1,62 +1,130 @@
 import image_tools as img_tl
 import numpy as np
+import scipy as sp
+from sklearn.decomposition import MiniBatchDictionaryLearning
+from sklearn.feature_extraction.image import extract_patches_2d
+from sklearn.feature_extraction.image import reconstruct_from_patches_2d
+import math
+
 from sklearn import linear_model
 import sys
-import math
+
+
+import matplotlib.pyplot as plt
 
 lowPatchSize = (3,3)
 highPatchSize = (6,6)
 representationSize = (3,3)
-atoms = 512
-lmbda = 0.1
+atoms = 100
+lmbda = 1
+iterations = 100
 
 def train(image_paths):
+
+    inner_stats = None
+
+    # train on each image
     
-    # make a Dh and Dl
-    Dl = np.random.rand(lowPatchSize[0]*lowPatchSize[1], atoms)
-    Dh = np.random.rand(highPatchSize[0]*highPatchSize[1], atoms)
-        
     for image_path in image_paths:
-        img = None
+    
+        # import image
         try:
             img = img_tl.importImage(image_path)
         except IOError:
             print("Error, an image could not be found in the images directory. Did you move it while the machine was training?")
             sys.exit()
+        
+        # prepare images to high and low res patches
         img_x, img_y = img.size
+        
+        if img_x % 2 == 1:
+            img = img.resize((img_x-1,img_y))
+            img_x -= 1
+        
+        if img_y % 2 == 1:
+            img = img.resize((img_x,img_y-1))
+            img_y -= 1
+        
         # ceil cause worried about 0 size
         img_low = img.resize((math.ceil(img_x*0.5), math.ceil(img_y*0.5)))
         
-        # dictionary learning test
-        dictionary_learning_test(
-            Dl, 
-            np.zeros((lowPatchSize[0]*lowPatchSize[1],1)), 
-            img_tl.get_patch(img_low, (64,64), lowPatchSize[0], lowPatchSize[1]))
         
-    
-def dictionary_learning_test(D, Z, X):
-    X = np.reshape(X, (lowPatchSize[0]*lowPatchSize[1], 1))
-    Xt = np.transpose(X)
-    Dt = np.transpose(D)
-    Zt = np.transpose(Z)
-    print(D)
-    print(Z)
-    print(X)
-    print(Xt*X)
-    print(Xt*D*Z)
-    print(Zt*Dt*D*Z)
-    print(Xt*X-2*Dt*Zt*X+Zt*Dt*D*Z)
-    
-    
+        # convert to patches
+        img = np.array(img)
+        img_low = np.array(img_low)
         
+        """ for now use only red channel """
+        img = img[:,:,0]
+        img_low = img_low[:,:,0]
+        
+        # print out the image for later testing
+        img_tl.save_as_image(img, "red.png")
+        img_tl.save_as_image(img_low, "red_low.png")
+        """-----------------------------"""
+        
+        print(img.shape)
+        print(img_low.shape)
+        
+        high_data = convertImageDataToPatches(img, highPatchSize, 2)
+        low_data = convertImageDataToPatches(img_low, lowPatchSize)
+        
+        high_data_size = high_data.shape[1]
+        low_data_size = low_data.shape[1]
+        
+        high_data *= 1/math.sqrt(high_data_size)
+        low_data *= 1/math.sqrt(low_data_size)
+        
+        print(high_data.shape)
+        print(low_data.shape)
+        
+        data = np.concatenate((high_data, low_data), axis = 1)
+        print(data.shape)
     
-def dictionary_learning(Dh, Dl, Z, N, M):
+        trainer = MiniBatchDictionaryLearning(
+        n_components = atoms,
+        alpha = lmbda*(1/high_data_size + 1/low_data_size),
+        n_iter = iterations,
+        n_jobs = -1,
+        verbose = 1)
+        if inner_stats:
+            trainer.inner_stats_ = inner_stats
     
-    # while not converged, loop
+        result = trainer.fit(data).components_
+        
+        np.save("models/sparse.npy", result)
+        
+        inner_stats = trainer.inner_stats_
+
+    print(result.shape)
+    resultHigh = result[:, :36]
+    resultLow = result[:, 36:]
+    plt.figure(figsize=(4.2, 4))
+    for i, comp in enumerate(resultHigh[:100]):
+        plt.subplot(10, 10, i + 1)
+        plt.imshow(comp.reshape(highPatchSize), cmap=plt.cm.gray_r,
+                   interpolation='nearest')
+        plt.xticks(())
+        plt.yticks(())
+    plt.show()
+    for i, comp in enumerate(resultLow[:100]):
+        plt.subplot(10, 10, i + 1)
+        plt.imshow(comp.reshape(lowPatchSize), cmap=plt.cm.gray_r,
+                   interpolation='nearest')
+        plt.xticks(())
+        plt.yticks(())
+    plt.show()
     
-    # fix D, update Z
-    # fix Z, update D
-    pass
+    
+def convertImageDataToPatches(img, patchSize, stride=1):
+    """ Converts image to a machine learning friendly format
+        Does this by converting 256 bit colour to a scale of 0 to 1,
+         turns it into patches with mean subtracted, and divides by std. dev
+    """
+    
+    temp = img_tl.get_patches(img/255, patchSize, stride)
+    temp = temp.reshape(temp.shape[0], -1)
+    return (temp - np.mean(temp, axis=0))/np.std(temp,axis=0)
+
 
 def super_size(Dh, Dl, Y):
     """Dh: high resolution dictionary
@@ -64,15 +132,3 @@ def super_size(Dh, Dl, Y):
        Y:  low resolution image
     """
     
-    maxX, maxY = Y.size
-    patchX, patchY = patchSize
-    
-    # for each patch
-    for i in range(maxY-patchY-1):
-        for j in range(maxX-patchX-1):
-            patch = img_tl.get_patch(Y, (i,j), patchY, patchX)
-            
-            meanPixelValue = np.mean(patch)
-            
-            
-            
